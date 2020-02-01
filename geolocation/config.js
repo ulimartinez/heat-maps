e4405d90a565f9b5531d71b4ebb970f845c3b189
Jorge <jorgehuertahu@gmail.com>
2020-02-01 12:08:03 -0700

    added my stuff (Jorge)

---
+++ geolocation/config.js
@@ -0,0 +1,90 @@
+// ADD THIS PART TO YOUR CODE
+var config = {}
+
+config.endpoint = "https://epso.documents.azure.com:443/";
+config.key = "mo1bJQV1abfMd7cV7qGjHLLPJGp8PuLNRFCsnErOLA42FdLuIaRQHYe5zRjwj9XTEW7357J7SfsESL6rGLOrxQ==";
+
+config.database = {
+  id: 'FamilyDatabase'
+}
+
+config.container = {
+  id: 'FamilyContainer'
+}
+
+config.items = {
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'Wakefield.7',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config;
