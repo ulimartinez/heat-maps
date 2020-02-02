6724f5ad41511cb666ddd808dcc09545679e4ab9
dateutli <daviidalberto@gmail.com>
2020-02-01 20:07:37 -0700

    fixed modal

---
+++ resources/safeZones.js
@@ -0,0 +1,129 @@
+let 
+safeZones = [
+  {
+    "host": "John G",
+    "capacity": 5,
+    "latitude": 31.858374267099162,
+    "longitude": -106.37165145777968
+  },
+  {
+    "host": "Aneth S",
+    "capacity": 4,
+    "latitude": 31.78705564057267,
+    "longitude": -106.33586901977597
+  },
+  {
+    "host": "Laura G",
+    "capacity": 5,
+    "latitude": 31.800296540544664,
+    "longitude": -106.37670343494294
+  },
+  {
+    "host": "Daniel P",
+    "capacity": 3,
+    "latitude": 31.85459473309614,
+    "longitude": -106.37401246856713
+  },
+  {
+    "host": "Gustavo O",
+    "capacity": 2,
+    "latitude": 31.83639702225926,
+    "longitude": -106.4254224126093
+  },
+  {
+    "host": "Rebeca E",
+    "capacity": 1,
+    "latitude": 31.804686741854514,
+    "longitude": -106.45651685797523
+  },
+  {
+    "host": "Suana K",
+    "capacity": 5,
+    "latitude": 31.859499768924717,
+    "longitude": -106.29237103979308
+  },
+  {
+    "host": "Carolina M",
+    "capacity": 4,
+    "latitude": 31.85612685998012,
+    "longitude": -106.34499430873315
+  },
+  {
+    "host": "Rose A",
+    "capacity": 3,
+    "latitude": 31.843881796374394,
+    "longitude": -106.44636021868794
+  },
+  {
+    "host": "Natalie G",
+    "capacity": 2,
+    "latitude": 31.807216402267237,
+    "longitude": -106.48057696130145
+  },
+  {
+    "host": "John G",
+    "capacity": 1,
+    "latitude": 31.7520523094285,
+    "longitude": -106.27760552918141
+  },
+  {
+    "host": "Melissa S",
+    "capacity": 5,
+    "latitude": 31.86317726735571,
+    "longitude": -106.39045832893328
+  },
+  {
+    "host": "Paulina P",
+    "capacity": 4,
+    "latitude": 31.79680161781297,
+    "longitude": -106.34323473524698
+  },
+  {
+    "host": "Jorge H",
+    "capacity": 3,
+    "latitude": 31.78756571904039,
+    "longitude": -106.50036593644064
+  },
+  {
+    "host": "David T",
+    "capacity": 2,
+    "latitude": 31.766234029043737,
+    "longitude": -106.48189171196793
+  },
+  {
+    "host": "Juan M",
+    "capacity": 1,
+    "latitude": 31.844667921170522,
+    "longitude": -106.5264716745859
+  },
+  {
+    "host": "Alex V",
+    "capacity": 5,
+    "latitude": 31.842875642479484,
+    "longitude": -106.49824907425082
+  },
+  {
+    "host": "Claudia R",
+    "capacity": 4,
+    "latitude": 31.839632100198944,
+    "longitude": -106.50896725458182
+  },
+  {
+    "host": "Paola G",
+    "capacity": 3,
+    "latitude": 31.784805574976346,
+    "longitude": -106.52894894404515
+  },
+  {
+    "host": "Diana L",
+    "capacity": 2,
+    "latitude": 31.753405963752165,
+    "longitude": -106.5251648071417
+  },
+  {
+    "host": "Ulises M",
+    "capacity": 1,
+    "latitude": 31.79841197835681,
+    "longitude": -106.39190502268721
+  }
+];
